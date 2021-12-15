d01f27351f80d8aebce0437222293dca10ef59c6
Geoff Shilling <gbs2@uw.edu>
2021-12-15 07:11:18 -0800

    Fixed for python3 byte io

--- BaseGZip.py
+++ BaseGZip.py
@@ -180,14 +180,14 @@ def decompress(input_file_name, output_file_or_file_name):
         # Read and discard a null-terminated string containing the filename
         while True:
             s = input_file.read(1)
-            if s == "\0":
+            if s == "\0" or s == b"\x00":
                 break
 
     if flag & FCOMMENT:
         # Read and discard a null-terminated string containing a comment
         while True:
             s = input_file.read(1)
-            if s == "\0":
+            if s == "\0" or s == b"\x00":
                 break
     if flag & FHCRC:
         input_file.read(2)  # Read & discard the 16-bit header CRC
