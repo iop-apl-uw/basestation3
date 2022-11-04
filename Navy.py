0c07f5bfb9e5278da22cf031ee79387590e1916e
Geoff Shilling <gbs2@uw.edu>
2022-11-03 18:09:32 -0700

    Code cleanup

--- Navy.py
+++ Navy.py
@@ -1,9 +1,10 @@
 #! /usr/bin/env python
+# -*- python-fmt -*-
 
-## 
-## Copyright (c) 2006, 2007, 2010, 2011, 2012, 2020 by University of Washington.  All rights reserved.
 ##
-## This file contains proprietary information and remains the 
+## Copyright (c) 2006-2022 by University of Washington.  All rights reserved.
+##
+## This file contains proprietary information and remains the
 ## unpublished property of the University of Washington. Use, disclosure,
 ## or reproduction is prohibited except as permitted by express written
 ## license agreement with the University of Washington.
@@ -23,16 +24,13 @@
 
 """Routines for creating Navy data products
 """
-import os
-import sys
-import string
-import time
-from numpy import *
 import math
-import BaseOpts
-from BaseLog import *
+
+import numpy as np
+
 import Utils
-import math
+
+from BaseLog import log_debug, log_warning
 
 #  Constants
 FEET_PER_METER = 3.2808
@@ -40,6 +38,7 @@ FEET_PER_METER = 3.2808
 #
 # Main entry functions
 #
+
 
 def kkyy(logfile, depth_v, tempc_v, salin_v, climb, n_points, kkyy_file):
     """Reduces the data and populates the KKYY file
@@ -52,14 +51,22 @@ def kkyy(logfile, depth_v, tempc_v, salin_v, climb, n_points, kkyy_file):
     """
     log_debug("n_points = %d" % n_points)
 
-    depth_reduced_v, tempc_reduced_v, salin_reduced_v = reduce_data(depth_v, tempc_v, salin_v, climb, n_points, False)
-    print_kkyy(logfile, depth_reduced_v, tempc_reduced_v, salin_reduced_v, climb, kkyy_file)
-    
+    depth_reduced_v, tempc_reduced_v, salin_reduced_v = reduce_data(
+        depth_v, tempc_v, salin_v, climb, n_points, False
+    )
+    print_kkyy(
+        logfile, depth_reduced_v, tempc_reduced_v, salin_reduced_v, climb, kkyy_file
+    )
+
+
 #
 # Format specific functions
 #
 
-def print_kkyy(logfile, depth_reduced_v, tempc_reduced_v, salin_reduced_v, climb, kkyy_file):
+
+def print_kkyy(
+    logfile, depth_reduced_v, tempc_reduced_v, salin_reduced_v, climb, kkyy_file
+):
     """Populates a KKYY file
 
     Input:
@@ -71,71 +78,62 @@ def print_kkyy(logfile, depth_reduced_v, tempc_reduced_v, salin_reduced_v, climb
     Output:
         None
     """
-    
-    #my ($LOG_object_r,$reduced_data_r,$climb) = @_;
-    #my ($fh,$filename,$subject) = prepare_output_file($LOG_object_r, "K", $climb);
-    
-    #my ($month,$day,$year,$hour,$min,$sec) = $LOG_object_r->param(($climb? 'end_date_time' : 'start_date_time')); # Assume UTC
-    #my (undef,$dlat,$dlng) = $LOG_object_r->spread([($climb ? 'GPS_END': 'GPS2'),[undef,"dlat","dlng"]]); # Encoded as +/-DDMM.SS
-    #$dlat = ddmm2dd($dlat);
-    #$dlng = ddmm2dd($dlng);
-    if(climb):
-        timestamp = logfile.data['$GPS'].datetime
-        ddlat = Utils.ddmm2dd(logfile.data['$GPS'].lat)
-        ddlon = Utils.ddmm2dd(logfile.data['$GPS'].lon)
+
+    if climb:
+        timestamp = logfile.data["$GPS"].datetime
+        ddlat = Utils.ddmm2dd(logfile.data["$GPS"].lat)
+        ddlon = Utils.ddmm2dd(logfile.data["$GPS"].lon)
     else:
-        timestamp = logfile.data['$GPS2'].datetime
-        ddlat = Utils.ddmm2dd(logfile.data['$GPS2'].lat)
-        ddlon = Utils.ddmm2dd(logfile.data['$GPS2'].lon)
+        timestamp = logfile.data["$GPS2"].datetime
+        ddlat = Utils.ddmm2dd(logfile.data["$GPS2"].lat)
+        ddlon = Utils.ddmm2dd(logfile.data["$GPS2"].lon)
 
-#     printf $fh ("KKYY %02d%02d%1d %02d%02d%1s %1d%05ld %06ld 888%1s%1s %03d%02d\n",
-#               $day,$month,$year%10,
-#               $hour,$min,
-#               '/',        # Encode in metric (meters/degC)
-#               WMO_3333($dlat,$dlng), # quadrant encoding
-#               abs($dlat*1000), # GPS is this accurate
-#               abs($dlng*1000),
-#               '7',        # Values at significant depths (code table 2262)
-#               '2',        # in-situ sensor, accuracy less tahn 0.02 PSU (code table 2263)
-#               830,        # WMO 1770 instrument type 'CTD'
-#               99,         # WMO 4770 recorder code 'inconnu'
-#              );
+    kkyy_file.write(
+        "KKYY %02d%02d%1d %02d%02d%1s %1d%05ld %06ld 888%1s%1s %03d%02d\n"
+        % (
+            timestamp.tm_mday,
+            timestamp.tm_mon,
+            timestamp.tm_year % 10,
+            timestamp.tm_hour,
+            timestamp.tm_min,
+            "/",  # Encode in metric (meters/degC)
+            WMO_3333(ddlat, ddlon),  # quadrant encoding
+            int(math.fabs(ddlat * 1000)),  # GPS is this accurate
+            int(math.fabs(ddlon * 1000)),
+            "7",  # Values at significant depths (code table 2262)
+            "2",  # in-situ sensor, accuracy less tahn 0.02 PSU (code table 2263)
+            830,  # WMO 1770 instrument type 'CTD'
+            99,  # WMO 4770 recorder code 'inconnu'
+        )
+    )
 
-    kkyy_file.write("KKYY %02d%02d%1d %02d%02d%1s %1d%05ld %06ld 888%1s%1s %03d%02d\n" %
-                (timestamp.tm_mday, timestamp.tm_mon, timestamp.tm_year%10, 
-                timestamp.tm_hour, timestamp.tm_min,
-                '/',        # Encode in metric (meters/degC)
-                WMO_3333(ddlat, ddlon), # quadrant encoding
-                int(math.fabs(ddlat*1000)), # GPS is this accurate
-                int(math.fabs(ddlon*1000)),
-                '7',        # Values at significant depths (code table 2262)
-                '2',        # in-situ sensor, accuracy less tahn 0.02 PSU (code table 2263)
-                830,        # WMO 1770 instrument type 'CTD'
-                99         # WMO 4770 recorder code 'inconnu'
-               ))
-    
-    #my $size = scalar(@{$reduced_data_r});
     size = len(depth_reduced_v)
 
     # Section 2 data
     for i in range(size):
         # Depth in meters (only when changing?)
         depth = depth_reduced_v[i]
-        
+
         # Navy wants temp in 100'ths of degree C, and rounded
         tempc = math.floor(tempc_reduced_v[i] * 100.0)
 
-        # For negative temperatures, 5000 shall be added to the absolute value of the temperature in hundredths of a degree Celsius
-        if(tempc < 0.0):
+        # For negative temperatures, 5000 shall be added to the absolute value of the
+        # temperature in hundredths of a degree Celsius
+        if tempc < 0.0:
             tempc = math.fabs(tempc) + 5000.0
 
         # Salin 100'th of PSU
         salin = salin_reduced_v[i] * 100.0
 
         try:
-            kkyy_file.write("2%04d 3%04d 4%04d\n" % (int(depth), int(tempc), int(salin)))
+            kkyy_file.write(
+                "2%04d 3%04d 4%04d\n" % (int(depth), int(tempc), int(salin))
+            )
         except ValueError:
-            log_warning("Error writing KKYY output (%s %s %s) - skipping" % (depth, tempc, salin))
+            log_warning(
+                "Error writing KKYY output (%s %s %s) - skipping"
+                % (depth, tempc, salin)
+            )
 
     # TODO What about hitting the bottom? write_jjvv_field("00000")?
 
@@ -144,15 +142,16 @@ def print_kkyy(logfile, depth_reduced_v, tempc_reduced_v, salin_reduced_v, climb
 
     # Section 4 data
     # call sign -- encode hull number
-    kkyy_file.write( "SG%03d\n" % int(logfile.data['$ID']))
+    kkyy_file.write("SG%03d\n" % int(logfile.data["$ID"]))
 
 
 #
 # Untility functions
 #
 
+
 def WMO_3333(lat, lon):
-    """ Enncode quadrant of globe
+    """Enncode quadrant of globe
 
     Input:
         lat,lon - latitude and longitude in dd
@@ -160,20 +159,21 @@ def WMO_3333(lat, lon):
     Output:
         Encoding
     """
-    if(lat > 0):
-        if(lon > 0):
-            #NE
+    if lat > 0:
+        if lon > 0:
+            # NE
             return 1
         else:
-            #NW
+            # NW
             return 7
     else:
-        if(lon > 0):
-            #SE
+        if lon > 0:
+            # SE
             return 3
         else:
-            #SW
+            # SW
             return 5
+
 
 def reduce_data(depth_v, tempc_v, salin_v, climb, n_points, english):
     """Reduces data columns to fit the Navy format
@@ -190,12 +190,12 @@ def reduce_data(depth_v, tempc_v, salin_v, climb, n_points, english):
     """
 
     size = len(depth_v)
-    skip_points = int(size/n_points)
-    if(skip_points == 0):
+    skip_points = int(size / n_points)
+    if skip_points == 0:
         skip_points = 1
-    
+
     # Assume all depths are in meters and temps are in degC
-    if (english):
+    if english:
         # The Navy wants Fahrenheit
         tempc_v = (9.0 / 5.0) * tempc_v + 32.0
         depth_v = depth_v * FEET_PER_METER
@@ -203,54 +203,42 @@ def reduce_data(depth_v, tempc_v, salin_v, climb, n_points, english):
     # TODO do we have to ensure the first point is at 0m?
     # For NITES II: "The second group after 88888 should be the first depth/temp pairing,
     # and should be for 0 depth."!
-    last_depth = -1 # Above the surface to force the first point out
-    reported_point = 0 # None so far
+    last_depth = -1  # Above the surface to force the first point out
+    reported_point = 0  # None so far
     depth_reduced = []
     tempc_reduced = []
     salin_reduced = []
 
     log_debug("skip_points = %s, size = %d" % (skip_points, size))
-    
+
     i = 0
-    #for (my $i = 0; $i < $size; $i = $i + $skip_points) {
-    while(i < size):
-        #index = ($climb ? $size - $i : $i); 
-        if(climb):
+    while i < size:
+        if climb:
             index = size - 1 - i
         else:
             index = i
         log_debug("i = %d, index = %d " % (i, index))
-        #my $depth = $depth_v->[$index];
-        #next unless defined $depth;
         depth = depth_v[index]
 
-        #my $temp = $tempc_v->[$index];
-        #next unless defined $temp;
         temp = tempc_v[index]
 
-        #my $salin = $salin_v->[$index];
-        #next unless defined $salin;
         salin = salin_v[index]
 
-    #my $this_depth = floor($depth);
         this_depth = math.floor(depth)
-        if (this_depth > last_depth): # only show increasing depths
+        if this_depth > last_depth:  # only show increasing depths
             # Record the last depth we emitted
-            #push(@reduced_data,[$depth,$svel,$temp,$salin]);
+            # push(@reduced_data,[$depth,$svel,$temp,$salin]);
             depth_reduced.append(depth)
             tempc_reduced.append(temp)
             salin_reduced.append(salin)
             last_depth = this_depth
             reported_point = reported_point + 1
-            #last if ($reported_point >= $n_points);
-            if(reported_point >= n_points):
+            # last if ($reported_point >= $n_points);
+            if reported_point >= n_points:
                 break
         i = i + skip_points
 
-    #print tempc_reduced
-    #print salin_reduced
-
-    depth_reduced_v = array(depth_reduced)
-    tempc_reduced_v = array(tempc_reduced)
-    salin_reduced_v = array(salin_reduced)
+    depth_reduced_v = np.array(depth_reduced)
+    tempc_reduced_v = np.array(tempc_reduced)
+    salin_reduced_v = np.array(salin_reduced)
     return [depth_reduced_v, tempc_reduced_v, salin_reduced_v]
