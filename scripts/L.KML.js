0704bb7715b80cba95f4e2bf49a3ec2d944230ad
Jason Gobat <jgobat@uw.edu>
2022-11-26 09:15:20 -0800

    example of using text vs description for popup KML

--- scripts/L.KML.js
+++ scripts/L.KML.js
@@ -266,7 +266,7 @@ L.Util.extend(L.KML, {
     if (el.length && el[0].childNodes.length) {
       name = el[0].childNodes[0].nodeValue;
     }
-    el = place.getElementsByTagName('description');
+    el = place.getElementsByTagName('text'); // description');
     for (i = 0; i < el.length; i++) {
       for (j = 0; j < el[i].childNodes.length; j++) {
         descr = descr + el[i].childNodes[j].nodeValue;
