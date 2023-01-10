16e43b0e1d8796b06a39cb5d372c7b4f155bc1e0
Jason Gobat <jgobat@uw.edu>
2023-01-09 21:34:32 -0800

    add markdown for glider (@) dive (#) plot ($) and links [](), @236#3$ts links to dive 3, glider 236 TS plot, $ctd creates link to change current glider to ctd plot, #100$pitch, etc.

--- scripts/chat.js
+++ scripts/chat.js
@@ -86,6 +86,83 @@
 
     }
 
+    function chatMarkdown(currentGlider, input) {
+        var link;
+        var glider;
+        var dive;
+        var plot;
+        const re = /(@(?<GDP_G>[0-9][0-9][0-9])#(?<GDP_D>[0-9]+)\$(?<GDP_P>[a-z0-9]+))|(@(?<GD_G>[0-9][0-9][0-9])#(?<GD_D>[0-9]+))|(@(?<G>[0-9][0-9][0-9]))|(#(?<D>[0-9]+))|(\$(?<P>[a-z0-9]+))|(#(?<DP_D>[0-9]+)\$(?<DP_P>[a-z0-9]+))|(?<link>\[(?<text>[\w\s\d]+)\]\((?<url>https?:\/\/([a-z0-9@#/.-]+))\))/g;
+        let matches = input.matchAll(re);
+        let out = '';
+        let cursorPos = 0;
+
+        console.log(input);
+        console.log(matches);
+
+        for (let match of matches) {
+            console.log(match);
+            const { groups: { GDP_G, GDP_D, GDP_P, GD_G, GD_D, G, D, P, DP_D, DP_P, link, text, url }, index } = match;
+            const full = match[0];
+            console.log(full);
+ 
+            out += input.substr(cursorPos, (index - cursorPos));
+            cursorPos = index + full.length;
+
+            console.log(GD_G);
+            console.log(GD_D);
+            console.log(match.groups);
+ 
+            if (GDP_G !== undefined) {
+                glider = parseInt(GDP_G);
+                dive   = parseInt(GDP_D);
+                plot   = GDP_P;
+                if (glider == currGlider) {
+                    out += `<span class="spanClick" onclick="setDive(${dive}); jumpScrollByName('${plot}');">${match[0]}</a>`;
+                }
+                else { 
+                    out += `<a href="/${glider}?dive=${dive}&plot=${plot}" target="${glider}">${match[0]}</a>`; 
+                }
+            }
+            else if (GD_G !== undefined) {
+                glider = parseInt(GD_G);
+                dive   = parseInt(GD_D);
+                if (glider == currGlider) {
+                    out += `<span class="spanClick" onclick="setDive(${dive});">${match[0]}</a>`;
+                }
+                else { 
+                    out += `<a href="/${glider}?dive=${dive}" target="${glider}">${full}</a>`; 
+                }
+            }
+            else if (DP_D !== undefined) {
+                dive = parseInt(DP_D);
+                plot = DP_P;
+                out += `<span class="spanClick" onclick="setDive(${dive}); jumpScrollByName('${plot}');">${match[0]}</a>`;
+            }
+            else if (P !== undefined) {
+                out += `<span class="spanClick" onclick="jumpScrollByName('${P}');">${match[0]}</a>`;
+            }
+            else if (D !== undefined) {
+                dive   = parseInt(D);
+                out += `<span class="spanClick" onclick="setDive(${dive});">${match[0]}</a>`;
+            }
+            else if (G !== undefined) {
+                glider = parseInt(G);
+                if (glider != currentGlider) {
+                    out += `<a href="/${glider}" target="${glider}">${full}</a>`; 
+                }
+            }
+            else if (link !== undefined) {
+                out += `<a href="${url}" target="_blank">${text}</a>`; 
+            }
+        }
+
+        if (cursorPos < input.length) {
+          out += input.substr(cursorPos, (input.length - cursorPos));
+        }
+
+        return out;
+    }
+
     function chatClose() {
         $('chatDiv').style.display = 'none';
     }
