5ccc72ed832c531647fe463544f3d19730ab6309
Geoff Shilling <gbs2@uw.edu>
2023-02-01 17:52:55 -0800

    Added depth tag to hover tips on motor plots

--- Plotting/MissionMotors.py
+++ Plotting/MissionMotors.py
@@ -2,7 +2,7 @@
 # -*- python-fmt -*-
 
 ##
-## Copyright (c) 2022 by University of Washington.  All rights reserved.
+## Copyright (c) 2022, 2023 by University of Washington.  All rights reserved.
 ##
 ## This file contains proprietary information and remains the
 ## unpublished property of the University of Washington. Use, disclosure,
@@ -78,86 +78,99 @@ def mission_motors(
             conn,
         ).sort_values("dive")
 
-        rdf = df[df['roll_rate'] != 0]
-        pdf = df[df['pitch_rate'] != 0]
-        vdf = df[df['vbd_rate'] != 0]
+        rdf = df[df["roll_rate"] != 0]
+        pdf = df[df["pitch_rate"] != 0]
+        vdf = df[df["vbd_rate"] != 0]
 
         fig = plotly.subplots.make_subplots(specs=[[{"secondary_y": True}]])
-        
-        fig.add_trace({
-            "x":rdf['dive'],
-            "y":rdf['roll_i'],
-            "name": "roll current",
-            "type": "scatter",
-            "mode": "markers",
-            "marker": {
-                "symbol": "triangle-down",
-                "color": "Cyan",
-            },
-            "hovertemplate": "dive %{x:.0f}<br>%{y:.2f}A<br><extra></extra>",
-        }, secondary_y=False)
-        fig.add_trace({
-            "x":rdf['dive'],
-            "y":rdf['roll_rate'],
-            "name": "roll rate",
-            "type": "scatter",
-            "mode": "markers",
-            "marker": {
-                "symbol": "triangle-down",
-                "color": "Magenta",
-            },
-            "hovertemplate": "dive %{x:.0f}<br>%{y:.2f}AD/s<br><extra></extra>",
-        }, secondary_y=True)
-        fig.add_trace({
-            "x":pdf['dive'],
-            "y":pdf['pitch_i'],
-            "name": "pitch current",
-            "type": "scatter",
-            "mode": "markers",
-            "marker": {
-                "symbol": "triangle-up",
-                "color": "DarkBlue",
-            },
-            "hovertemplate": "dive %{x:.0f}<br>%{y:.2f}A<br><extra></extra>",
-        })
-        fig.add_trace({
-            "x":pdf['dive'],
-            "y":pdf['pitch_rate'],
-            "name": "pitch rate",
-            "type": "scatter",
-            "mode": "markers",
-            "marker": {
-                "symbol": "triangle-up",
-                "color": "Red",
-            },
-            "hovertemplate": "dive %{x:.0f}<br>%{y:.2f}AD/s<br><extra></extra>",
-        }, secondary_y=True)
 
-        fig.update_layout({
-            "xaxis": {
-                "title": "dive",
-                "showgrid": True,
+        fig.add_trace(
+            {
+                "x": rdf["dive"],
+                "y": rdf["roll_i"],
+                "name": "roll current",
+                "type": "scatter",
+                "mode": "markers",
+                "marker": {
+                    "symbol": "triangle-down",
+                    "color": "Cyan",
+                },
+                "hovertemplate": "dive %{x:.0f}<br>%{y:.2f}A<br><extra></extra>",
             },
-            "yaxis": {
-                "title": "current(A)",
-                "showgrid": True,
+            secondary_y=False,
+        )
+        fig.add_trace(
+            {
+                "x": rdf["dive"],
+                "y": rdf["roll_rate"],
+                "name": "roll rate",
+                "type": "scatter",
+                "mode": "markers",
+                "marker": {
+                    "symbol": "triangle-down",
+                    "color": "Magenta",
+                },
+                "hovertemplate": "dive %{x:.0f}<br>%{y:.2f}AD/s<br><extra></extra>",
             },
-            "yaxis2": {
-                "title": "rate(AD/s)",
-                "showgrid": False,
+            secondary_y=True,
+        )
+        fig.add_trace(
+            {
+                "x": pdf["dive"],
+                "y": pdf["pitch_i"],
+                "name": "pitch current",
+                "type": "scatter",
+                "mode": "markers",
+                "marker": {
+                    "symbol": "triangle-up",
+                    "color": "DarkBlue",
+                },
+                "hovertemplate": "dive %{x:.0f}<br>%{y:.2f}A<br><extra></extra>",
+            }
+        )
+        fig.add_trace(
+            {
+                "x": pdf["dive"],
+                "y": pdf["pitch_rate"],
+                "name": "pitch rate",
+                "type": "scatter",
+                "mode": "markers",
+                "marker": {
+                    "symbol": "triangle-up",
+                    "color": "Red",
+                },
+                "hovertemplate": "dive %{x:.0f}<br>%{y:.2f}AD/s<br><extra></extra>",
             },
-            "title": {
-                "text": "pitch and roll motor diagnostics",
-                "xanchor": "center",
-                "yanchor": "top",
-                "x": 0.5,
-                "y": 0.95,
-            },
-            "margin": {
-                "t": 100,
-                "b": 25,
-            },
-        })
+            secondary_y=True,
+        )
+
+        fig.update_layout(
+            {
+                "xaxis": {
+                    "title": "dive",
+                    "showgrid": True,
+                },
+                "yaxis": {
+                    "title": "current(A)",
+                    "showgrid": True,
+                },
+                "yaxis2": {
+                    "title": "rate(AD/s)",
+                    "showgrid": False,
+                },
+                "title": {
+                    "text": "pitch and roll motor diagnostics",
+                    "xanchor": "center",
+                    "yanchor": "top",
+                    "x": 0.5,
+                    "y": 0.95,
+                },
+                "margin": {
+                    "t": 100,
+                    "b": 25,
+                },
+            }
+        )
 
         figs_list.append(fig)
         file_list.append(
@@ -169,71 +182,85 @@ def mission_motors(
         )
 
         fig = plotly.subplots.make_subplots(specs=[[{"secondary_y": True}]])
-       
-        pumpdf = vdf[vdf['vbd_rate'] < 0] 
-        bleeddf = vdf[vdf['vbd_rate'] > 0] 
-        fig.add_trace({
-            "x":pumpdf['dive'],
-            "y":pumpdf['vbd_i'],
-            "name": "pump current",
-            "type": "scatter",
-            "mode": "markers",
-            "marker": {
-                "symbol": "triangle-down",
-                "color": "Blue",
-            },
-            "hovertemplate": "dive %{x:.0f}<br>%{y:.2f}A<br><extra></extra>",
-        }, secondary_y=False)
-        fig.add_trace({
-            "x":pumpdf['dive'],
-            "y":pumpdf['vbd_rate'],
-            "name": "pump rate",
-            "type": "scatter",
-            "mode": "markers",
-            "marker": {
-                "symbol": "triangle-down",
-                "color": "Red",
-            },
-            "hovertemplate": "dive %{x:.0f}<br>%{y:.2f}AD/s<br><extra></extra>",
-        }, secondary_y=True)
-        fig.add_trace({
-            "x":bleeddf['dive'],
-            "y":bleeddf['vbd_rate'],
-            "name": "bleed rate",
-            "type": "scatter",
-            "mode": "markers",
-            "marker": {
-                "symbol": "triangle-up",
-                "color": "Magenta",
-            },
-            "hovertemplate": "dive %{x:.0f}<br>%{y:.2f}AD/s<br><extra></extra>",
-        }, secondary_y=True)
 
-        fig.update_layout({
-            "xaxis": {
-                "title": "dive",
-                "showgrid": True,
+        pumpdf = vdf[vdf["vbd_rate"] < 0]
+        bleeddf = vdf[vdf["vbd_rate"] > 0]
+        fig.add_trace(
+            {
+                "x": pumpdf["dive"],
+                "y": pumpdf["vbd_i"],
+                "meta": pumpdf["depth"],
+                "name": "Pump Current",
+                "type": "scatter",
+                "mode": "markers",
+                "marker": {
+                    "symbol": "triangle-down",
+                    "color": "Blue",
+                },
+                "hovertemplate": "Dive %{x:.0f}<br>Current %{y:.2f}A<br>Depth %{meta:.2f} m<extra></extra>",
             },
-            "yaxis": {
-                "title": "current(A)",
-                "showgrid": True,
+            secondary_y=False,
+        )
+        fig.add_trace(
+            {
+                "x": pumpdf["dive"],
+                "y": pumpdf["vbd_rate"],
+                "meta": pumpdf["depth"],
+                "name": "Pump Rate",
+                "type": "scatter",
+                "mode": "markers",
+                "marker": {
+                    "symbol": "triangle-down",
+                    "color": "Red",
+                },
+                "hovertemplate": "Dive %{x:.0f}<br>Rate %{y:.2f} AD/s<br>Depth %{meta:.2f} m<extra></extra>",
             },
-            "yaxis2": {
-                "title": "rate(AD/s)",
-                "showgrid": False,
+            secondary_y=True,
+        )
+        fig.add_trace(
+            {
+                "x": bleeddf["dive"],
+                "y": bleeddf["vbd_rate"],
+                "meta": pumpdf["depth"],
+                "name": "Bleed Rate",
+                "type": "scatter",
+                "mode": "markers",
+                "marker": {
+                    "symbol": "triangle-up",
+                    "color": "Magenta",
+                },
+                "hovertemplate": "Dive %{x:.0f}<br>Rate %{y:.2f} AD/s<br>Depth %{meta:.2f} m<extra></extra>",
             },
-            "title": {
-                "text": "pump and bleed diagnostics",
-                "xanchor": "center",
-                "yanchor": "top",
-                "x": 0.5,
-                "y": 0.95,
-            },
-            "margin": {
-                "t": 100,
-                "b": 25,
-            },
-        })
+            secondary_y=True,
+        )
+
+        fig.update_layout(
+            {
+                "xaxis": {
+                    "title": "Dive",
+                    "showgrid": True,
+                },
+                "yaxis": {
+                    "title": "Current (A)",
+                    "showgrid": True,
+                },
+                "yaxis2": {
+                    "title": "Rate (AD/s)",
+                    "showgrid": False,
+                },
+                "title": {
+                    "text": "Pump and Bleed Diagnostics",
+                    "xanchor": "center",
+                    "yanchor": "top",
+                    "x": 0.5,
+                    "y": 0.95,
+                },
+                "margin": {
+                    "t": 100,
+                    "b": 25,
+                },
+            }
+        )
 
         figs_list.append(fig)
         file_list.append(
@@ -245,44 +272,50 @@ def mission_motors(
         )
 
         fig = plotly.graph_objects.Figure()
-       
-        fig.add_trace({
-            "x":pumpdf['dive'],
-            "y":pumpdf['vbd_eff'],
-            "name": "VBD efficiency",
-            "type": "scatter",
-            "mode": "markers",
-            "marker": {
-                "color": pumpdf['depth'],
-                "colorbar": {
-                    "title": "depth",
-                    "len": 0.8,
-                },
-                "colorscale": "Jet",
-            },
-        })
 
-        fig.update_layout({
-            "xaxis": {
-                "title": "dive",
-                "showgrid": True,
-            },
-            "yaxis": {
-                "title": "efficiency",
-                "showgrid": True,
-            },
-            "title": {
-                "text": "VBD efficiency",
-                "xanchor": "center",
-                "yanchor": "top",
-                "x": 0.5,
-                "y": 0.95,
-            },
-            "margin": {
-                "t": 100,
-                "b": 25,
-            },
-        })
+        fig.add_trace(
+            {
+                "x": pumpdf["dive"],
+                "y": pumpdf["vbd_eff"],
+                "meta": pumpdf["depth"],
+                "name": "VBD Efficiency",
+                "type": "scatter",
+                "mode": "markers",
+                "marker": {
+                    "color": pumpdf["depth"],
+                    "colorbar": {
+                        "title": "depth",
+                        "len": 0.8,
+                    },
+                    "colorscale": "Jet",
+                },
+                "hovertemplate": "VBD Efficiency<br>Dive %{x:.0f}<br>%{y:.2f} Efficiency<br>Depth %{meta:.2f} m<br><extra></extra>",
+            }
+        )
+
+        fig.update_layout(
+            {
+                "xaxis": {
+                    "title": "Dive",
+                    "showgrid": True,
+                },
+                "yaxis": {
+                    "title": "Efficiency",
+                    "showgrid": True,
+                },
+                "title": {
+                    "text": "VBD Efficiency",
+                    "xanchor": "center",
+                    "yanchor": "top",
+                    "x": 0.5,
+                    "y": 0.95,
+                },
+                "margin": {
+                    "t": 100,
+                    "b": 25,
+                },
+            }
+        )
 
         figs_list.append(fig)
         file_list.append(
@@ -293,7 +326,6 @@ def mission_motors(
             )
         )
 
-
         return (figs_list, file_list)
 
     except:
@@ -303,4 +335,3 @@ def mission_motors(
             pdb.post_mortem(traceb)
         log_error("Could not fetch needed columns", "exc")
         return ([], [])
-
