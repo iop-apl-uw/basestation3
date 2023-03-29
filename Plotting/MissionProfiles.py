fe351becbbbaa2e233258dfc576d7587eb673d9e
Jason Gobat <jgobat@uw.edu>
2023-03-28 17:54:57 -0700

    units specifier for section plots in sections.yml

--- Plotting/MissionProfiles.py
+++ Plotting/MissionProfiles.py
@@ -155,6 +155,7 @@ def mission_profiles(
             cmap = getValue(x, 'colormap', sk, vk, 'thermal')
             zmin = getValue(x, 'min', sk, vk, None)
             zmax = getValue(x, 'max', sk, vk, None)
+            units = getValue(x, 'units', sk, vk, None)
 
             if stop == -1 or stop >= latest:
                 stop = latest
@@ -182,7 +183,7 @@ def mission_profiles(
                             "family": "Raleway",
                             "size": 12,
                             "color": "white"
-                        }
+                        },
                      }
 
             props = {
@@ -192,7 +193,15 @@ def mission_profiles(
                         'contours_coloring': 'heatmap',
                         'colorscale':        cmocean_to_plotly(cmap, 100),
                         'connectgaps':       True,
-                        'contours':          contours
+                        'contours':          contours,
+                        "colorbar": {
+                            "title": {
+                                "text": units,
+                                "side": "top",
+                            },
+                            # "thickness": 0.02,
+                            # "thicknessmode": "fraction",
+                        },
                     }
 
             if zmin is not None:
@@ -203,14 +212,22 @@ def mission_profiles(
             fig.add_trace(plotly.graph_objects.Contour( **props ) )
 
             title_text = f"{mission_str}<br>{vk}<br>section {sk}: {start}-{stop}"
-            
+ 
             fig.update_layout(
                 {
                     "xaxis": {
                         "title": "dive",
+                        # "title": units,
                         "showgrid": False,
                         "autorange": "reversed" if flip else True,
                     },
+                    "xaxis2": {
+                        "title": units,
+                        "showgrid": False,
+                        "side": "top",
+                        "overlaying": "x1",
+                    },
+
                     "yaxis": {
                         "title": "depth",
                         "showgrid": False,
