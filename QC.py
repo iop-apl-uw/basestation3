434a18e016aef8c9f57a9c819c3b67260b9c3986
Geoff Shilling <gbs2@uw.edu>
2023-04-01 09:35:58 -0700

    Limit QC warnings

--- QC.py
+++ QC.py
@@ -2,21 +2,21 @@
 # -*- python-fmt -*-
 
 ## Copyright (c) 2023  University of Washington.
-## 
+##
 ## Redistribution and use in source and binary forms, with or without
 ## modification, are permitted provided that the following conditions are met:
-## 
+##
 ## 1. Redistributions of source code must retain the above copyright notice, this
 ##    list of conditions and the following disclaimer.
-## 
+##
 ## 2. Redistributions in binary form must reproduce the above copyright notice,
 ##    this list of conditions and the following disclaimer in the documentation
 ##    and/or other materials provided with the distribution.
-## 
+##
 ## 3. Neither the name of the University of Washington nor the names of its
 ##    contributors may be used to endorse or promote products derived from this
 ##    software without specific prior written permission.
-## 
+##
 ## THIS SOFTWARE IS PROVIDED BY THE UNIVERSITY OF WASHINGTON AND CONTRIBUTORS “AS
 ## IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 ## IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -863,7 +863,8 @@ class ProfileDirectives:
                     self.comments = []  # reset
                     return None  # This line does not apply
         except ValueError:
-            log_warning(f"Unknown dive specifier '{dive_spec}' in '{line}'")
+            qc_warn_str = f"Unknown dive specifier '{dive_spec}' in '{line}'"
+            log_warning(qc_warn_str, alert=qc_warn_str, max_count=1)
 
         # this function applies to all or this dive_number
         function_tag = values[1]
