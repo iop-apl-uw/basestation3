34c1f42e83bccb93f55122b8472cd6200e8d3fbf
Geoff Shilling <gbs2@uw.edu>
2023-04-24 10:00:24 -0700

    Fixed name for second compass heading

--- Plotting/DiveCompassCompare.py
+++ Plotting/DiveCompassCompare.py
@@ -2,21 +2,21 @@
 # -*- python-fmt -*-
 
 ## Copyright (c) 2023  University of Washington.
-## 
+##
 ## Redistribution and use in source and binary forms, with or without
 ## modification, are permitted provided that the following conditions are met:
-## 
+##
 ## 1. Redistributions of source code must retain the above copyright notice, this
 ##    list of conditions and the following disclaimer.
-## 
+##
 ## 2. Redistributions in binary form must reproduce the above copyright notice,
 ##    this list of conditions and the following disclaimer in the documentation
 ##    and/or other materials provided with the distribution.
-## 
+##
 ## 3. Neither the name of the University of Washington nor the names of its
 ##    contributors may be used to endorse or promote products derived from this
 ##    software without specific prior written permission.
-## 
+##
 ## THIS SOFTWARE IS PROVIDED BY THE UNIVERSITY OF WASHINGTON AND CONTRIBUTORS “AS
 ## IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 ## IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -413,7 +413,7 @@ def plot_compass_compare(
         {
             "y": second_compass_heading,
             "x": second_compass_time,
-            "name": "Heading (deg) - truck ",
+            "name": f"Heading (deg) - {second_compass_tag}",
             "type": "scatter",
             "xaxis": "x1",
             "yaxis": "y2",
