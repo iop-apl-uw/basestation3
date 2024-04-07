cbe4743336257a8fc0e57d54d2e8e2903dcb2096
Geoff Shilling <gbs2@uw.edu>
2024-04-07 09:18:48 -0700

    Calc averaged lon if not available in nc file

--- Plotting/DiveTS.py
+++ Plotting/DiveTS.py
@@ -1,22 +1,22 @@
 #! /usr/bin/env python
 # -*- python-fmt -*-
 
-## Copyright (c) 2023  University of Washington.
-## 
+## Copyright (c) 2023, 2024  University of Washington.
+##
 ## Redistribution and use in source and binary forms, with or without
 ## modification, are permitted provided that the following conditions are met:
-## 
+##
 ## 1. Redistributions of source code must retain the above copyright notice, this
 ##    list of conditions and the following disclaimer.
-## 
+##
 ## 2. Redistributions in binary form must reproduce the above copyright notice,
 ##    this list of conditions and the following disclaimer in the documentation
 ##    and/or other materials provided with the distribution.
-## 
+##
 ## 3. Neither the name of the University of Washington nor the names of its
 ##    contributors may be used to endorse or promote products derived from this
 ##    software without specific prior written permission.
-## 
+##
 ## THIS SOFTWARE IS PROVIDED BY THE UNIVERSITY OF WASHINGTON AND CONTRIBUTORS “AS
 ## IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 ## IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -43,6 +43,7 @@ import seawater
 if typing.TYPE_CHECKING:
     import BaseOpts
 
+import MakeDiveProfiles
 import PlotUtils
 import PlotUtilsPlotly
 import QC
@@ -143,7 +144,14 @@ def plot_TS(
                     pressure = pressure[good_mask]
 
             if base_opts.use_gsw:
-                avg_longitude = dive_nc_file.variables["avg_longitude"].getValue()
+                if "avg_longitude" in dive_nc_file.variables:
+                    avg_longitude = dive_nc_file.variables["avg_longitude"].getValue()
+                else:
+                    # Older basestations didn't calcuate this value
+                    avg_longitude = MakeDiveProfiles.avg_longitude(
+                        dive_nc_file.variables["log_gps_lon"][1],
+                        dive_nc_file.variables["log_gps_lon"][2],
+                    )
                 avg_latitude = dive_nc_file.variables["avg_latitude"].getValue()
 
         except KeyError as e:
