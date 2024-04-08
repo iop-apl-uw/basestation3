f4ee2bccf9955da3d51479b71719aa7535cccba6
Jason Gobat <jgobat@uw.edu>
2024-04-08 02:24:03 +0000

    change permissions on db file at commissioning

--- Commission.py
+++ Commission.py
@@ -233,6 +233,8 @@ def main():
     base_opts.instrument_id = glider_id
     base_opts.mission_dir = glider_path
     BaseDB.createDB(base_opts)
+    db_file_name = os.path.join(glider_path, "%s.db" % glider)
+    syscall("chown %s.%s %s" % (glider, base_opts.home_dir_group, db_file_name))
 
     # syscall("chown pilot %s/cmdfile" % glider_path)
     # syscall("echo %s | passwd %s --stdin" % (passwd, glider))
