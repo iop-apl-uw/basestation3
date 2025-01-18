3cb3f9e0e139ba7464dc5ade9f883df321349634
Geoff Shilling <gbs2@uw.edu>
2025-01-17 18:11:31 -0800

    Added pmar disk use plot

--- Plotting/MissionDisk.py
+++ Plotting/MissionDisk.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 # -*- python-fmt -*-
 
-## Copyright (c) 2023, 2024  University of Washington.
+## Copyright (c) 2023, 2024, 2025  University of Washington.
 ##
 ## Redistribution and use in source and binary forms, with or without
 ## modification, are permitted provided that the following conditions are met:
@@ -28,11 +28,11 @@
 ## LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
 ## OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-""" Plots disk stats
-"""
+"""Plots disk stats"""
+
 # TODO: This can be removed as of python 3.11
 from __future__ import annotations
-
+import pdb
 import warnings
 import typing
 
@@ -65,7 +65,7 @@ def mission_disk(
     if not generate_plots:
         return ([], [])
 
-    if dbcon == None:
+    if dbcon is None:
         conn = Utils.open_mission_database(base_opts, ro=True)
         if not conn:
             log_error("Could not open mission database")
@@ -96,18 +96,14 @@ def mission_disk(
         ).sort_values("dive")
     except Exception:
         log_error("Could not fetch needed columns", "exc")
-        if dbcon == None:
+        if dbcon is None:
             conn.close()
             log_info("mission_disk db closed")
         return ([], [])
 
-    if dbcon == None:
+    if dbcon is None:
         conn.close()
         log_info("mission_disk db closed")
-
-    y_offset = -0.08
-
-    # l_annotations = []
 
     sd_free_est = ""
     sc_free_est = ""
@@ -134,17 +130,6 @@ def mission_disk(
             m, b = np.polyfit(df["dive"].to_numpy(), df["SD_free"].to_numpy(), 1)
 
         sd_free_est = f"<br>based on SD free, {-b/m:.0f} dives until full"
-        # y_offset += -0.02
-        # l_annotations.append(
-        #     {
-        #         "text": sd_free_est,
-        #         "showarrow": False,
-        #         "xref": "paper",
-        #         "yref": "paper",
-        #         "x": 0.0,
-        #         "y": y_offset,
-        #     }
-        # )
 
     if "SD_files" in df.columns:
         fig.add_trace(
@@ -265,6 +250,165 @@ def mission_disk(
         PlotUtilsPlotly.write_output_files(
             base_opts,
             "eng_mission_disk",
+            fig,
+        ),
+    )
+
+
+pmar_free_color_map = (
+    "red",
+    "orange",
+    "darkgoldenrod",
+    "green",
+    "blue",
+    "indigo",
+    "violet",
+    # "black",
+)
+
+
+@plotmissionsingle
+def mission_pmar_disk(
+    base_opts: BaseOpts.BaseOptions,
+    mission_str: list,
+    dive=None,
+    generate_plots=True,
+    dbcon=None,
+) -> tuple[list, list]:
+    """Plots disk stats"""
+
+    if not generate_plots:
+        return ([], [])
+
+    if dbcon is None:
+        conn = Utils.open_mission_database(base_opts, ro=True)
+        if not conn:
+            log_error("Could not open mission database")
+            return ([], [])
+        log_info("mission_disk db opened (ro)")
+    else:
+        conn = dbcon
+
+    res = conn.cursor().execute("PRAGMA table_info(dives)")
+    columns = [i[1] for i in res]
+
+    qcols = list(filter(lambda x: x.startswith("log_PM_FREEKB"), columns))
+
+    if len(qcols) == 0:
+        return ([], [])
+
+    qstr = ",".join(qcols)
+
+    df = None
+    try:
+        df = pd.read_sql_query(
+            f"SELECT dive,{qstr} from dives",
+            conn,
+        ).sort_values("dive")
+    except Exception:
+        log_error("Could not fetch needed columns", "exc")
+        if dbcon is None:
+            conn.close()
+            log_info("mission_disk db closed")
+        return ([], [])
+
+    if dbcon is None:
+        conn.close()
+        log_info("mission_disk db closed")
+
+    fig = plotly.graph_objects.Figure()
+    fields = [i for i in df if i.startswith("log_PM")]
+    for field in fields:
+        drive_id = int(field.split("_")[-1])
+        fig.add_trace(
+            {
+                "name": f"PM {drive_id:02d} free kB",
+                "x": df["dive"],
+                "y": df[field],
+                "yaxis": "y1",
+                "mode": "lines",
+                "line": {
+                    "dash": "solid",
+                    "color": pmar_free_color_map[drive_id],
+                    "width": 1,
+                },
+                "hovertemplate": "Dive %{x}<br>PM "
+                + f"{drive_id:02d}"
+                + " free space %{y} kb<extra></extra>",
+            }
+        )
+    pm_free_est = ""
+    if len(fields) > 1:
+        pm_tot = df[fields].sum(axis=1)
+        fig.add_trace(
+            {
+                "name": "PM Total free kB",
+                "x": df["dive"],
+                "y": pm_tot,
+                "yaxis": "y1",
+                "mode": "lines",
+                "line": {
+                    "dash": "solid",
+                    "color": "black",
+                    "width": 1,
+                },
+                "hovertemplate": "Dive %{x}<br>PM Total free space %{y} kb<extra></extra>",
+            }
+        )
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore", category=np.RankWarning)
+            m, b = np.polyfit(df["dive"].to_numpy(), pm_tot.to_numpy(), 1)
+
+        pm_free_est = f"<br>based on PM Total free, {-b/m:.0f} dives until full"
+    else:
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore", category=np.RankWarning)
+            m, b = np.polyfit(df["dive"].to_numpy(), df[fields[0]].to_numpy(), 1)
+
+        pm_free_est = f"<br>based on PM free, {-b/m:.0f} dives until full"
+
+    fig.update_layout(
+        {
+            "xaxis": {
+                # "title": "Dive Number",
+                "title": f"Dive Number{pm_free_est}",
+                "showgrid": True,
+                "domain": [0, 0.95],
+            },
+            "yaxis": {
+                "title": "free space (kB)",
+                "showgrid": True,
+                "tickformat": ".0f",
+            },
+            "yaxis2": {
+                "title": "count",
+                "overlaying": "y1",
+                "side": "right",
+                "showgrid": False,
+                "tickformat": ".0f",
+            },
+            "title": {
+                "text": f"{mission_str}<br>Disk usage",
+                "xanchor": "center",
+                "yanchor": "top",
+                "x": 0.5,
+                "y": 0.95,
+            },
+            "legend": {
+                "x": 1.05,
+                "y": 1,
+            },
+            "margin": {
+                "b": 120,
+            },
+            # "annotations": tuple(l_annotations),
+        },
+    )
+    return (
+        [fig],
+        PlotUtilsPlotly.write_output_files(
+            base_opts,
+            "eng_mission_pmar_disk",
             fig,
         ),
     )
