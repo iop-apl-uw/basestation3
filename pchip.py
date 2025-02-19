bf31d8f85d5c55463f94b38f6e6512fcdb3e06fd
Geoff Shilling <gbs2@uw.edu>
2025-02-19 14:31:21 -0800

    Removed "from numpy import *"

--- pchip.py
+++ pchip.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 
-## Copyright (c) 2023  University of Washington.
+## Copyright (c) 2023, 2025  University of Washington.
 ## 
 ## Redistribution and use in source and binary forms, with or without
 ## modification, are permitted provided that the following conditions are met:
@@ -27,24 +27,18 @@
 ## LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
 ## OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-# All this for __main__ below
-import os
-import sys
-import traceback
-import string
-import time
+import numpy as np
 
-from numpy import *
 
 def pchip(x,y,xx,expected_index=None): # pchip_m
     """Interpolate xx using piecewise cubic Hermite polynomials based on x, y
     Input:
-    	x - input array of real x values, assumed ascending 
+        x - input array of real x values, assumed ascending 
         y - input array of real y values corresponding to xi values
         xx - input array of real x points to interpolate
 
     Returns:
-    	yy - interpolated values of x
+        yy - interpolated values of x
 
     Raises:
     
@@ -85,25 +79,27 @@ def pchip(x,y,xx,expected_index=None): # pchip_m
     # M: y = y';
     # M: xx = xx';
     # NOTE: if we are passed x or y as integers division computations for w1 and w2 expressions lead to zeros and hence bad answers
-    x = array(x, float64); y = array(y, float64); xx = array(xx, float64)
+    x = np.array(x, np.float64)
+    y = np.array(y, np.float64)
+    xx = np.array(xx, np.float64)
     # M: pchip.m
     # M: n = length(x); h = diff(x); m = prod(sizey);
     n = len(x)
     # hard-wired offsets adjusted for 0-based rather than 1-based indexing
     # In the following we convert to python addressing by changing n+/-x to nn+/-x
     nn = n - 1 # last addressible index in x and y, etc. (would be n for matlab).
-    h = diff(x)
+    h = np.diff(x)
     # UNUSED M: m = 1 # m = prod(sizey)  = prod(1) = 1
     sizey = len(y) # size(y) != length(y) in general for matlab but it is the case for vectors
     # NOTE: Renamed del in original to delta here to avoid del() python function
     # M: delta = diff(y,1,2)./repmat(h,m,1);
     # M: delta = diff(y,1,2)./h;
-    delta = diff(y)/h
-    d = zeros(sizey)
+    delta = np.diff(y)/h
+    d = np.zeros(sizey)
     # M: d = pchipslopes(x,y,delta); % open-coded function call
     if (n == 2):
         # M: d = repmat(delta(1),size(y)); # replicate delta(1) and tile size(y) times
-        d = delta[0]*ones(sizey) # equivalent?
+        d = delta[0]*np.ones(sizey) # equivalent?
     else:
         #  Slopes at interior points.
         #  d(k) = weighted average of delta(k-1) and delta(k) when they have the same sign.
@@ -111,12 +107,12 @@ def pchip(x,y,xx,expected_index=None): # pchip_m
 
         # DONE see above M: d = zeros(size(y));
         k = [] # initialize in scope
-        if all(isreal(delta)):
+        if all(np.isreal(delta)):
             # M: k = find(sign(delta(1:n-2)).*sign(delta(2:n-1)) > 0)
-            k = nonzero(sign(delta[0:n-2])*sign(delta[1:n-1]) > 0)
+            k = np.nonzero(np.sign(delta[0:n-2])*np.sign(delta[1:n-1]) > 0)
         else:
             # M: k = find(~(delta(1:n-2) == 0 & delta(2:n-1) == 0))
-            k = nonzero(not(delta[0:n-2] == 0 and delta[2:n-1] == 0))
+            k = np.nonzero(not(delta[0:n-2] == 0 and delta[2:n-1] == 0))
         k = k[0] # fetch equivalent python result; where we changed inflection
         # CONSIDER k1 = k+1
         # DONE see above M: h = diff(x)
@@ -128,39 +124,39 @@ def pchip(x,y,xx,expected_index=None): # pchip_m
         # M: w2 = (hs+h(k+1))/(3*hs)
         w2 = (hs+h[k+1])/(3*hs)
         # M: dmax = max(abs(delta(k)), abs(delta(k+1)))
-        dmax = maximum(abs(delta[k]), abs(delta[k+1]))
+        dmax = np.maximum(abs(delta[k]), abs(delta[k+1]))
         # M: dmin = min(abs(delta(k)), abs(delta(k+1)))
-        dmin = minimum(abs(delta[k]), abs(delta[k+1]))
+        dmin = np.minimum(abs(delta[k]), abs(delta[k+1]))
         # M: cc = w1*(delta(k)/dmax) + w2*(delta(k+1)/dmax)
         cc = w1*(delta[k]/dmax) + w2*(delta[k+1]/dmax)
         # Is cc ever complex for our application?  Shouldn't be....
         # M: d(k+1) = dmin/conj(cc)
-        d[k+1] = dmin/conjugate(cc)
+        d[k+1] = dmin/np.conjugate(cc)
 
         #  Slopes at end points.
         #  Set d(1) and d(n) via non-centered, shape-preserving three-point formulae.
 
         # M: d(1) = ((2*h(1)+h(2))*delta(1) - h(1)*delta(2))/(h(1)+h(2));
-        d[0] = ((2*h[0]+h[1])*delta[0] - h[0]*delta[1])/(h[0]+h[1]);
+        d[0] = ((2*h[0]+h[1])*delta[0] - h[0]*delta[1])/(h[0]+h[1])
         # M: if isreal(d) && (sign(d(1)) ~= sign(del(1)))
-        if all(isreal(d)) and (sign(d[0]) != sign(delta[0])):
+        if all(np.isreal(d)) and (np.sign(d[0]) != np.sign(delta[0])):
             # M: d(1) = 0
             d[0] = 0
         # M: elseif (sign(del(1)) ~= sign(del(2))) && (abs(d(1)) > abs(3*del(1)))
-        elif (sign(delta[0]) != sign(delta[1])) and (abs(d[0]) > abs(3*delta[0])):
+        elif (np.sign(delta[0]) != np.sign(delta[1])) and (abs(d[0]) > abs(3*delta[0])):
             # M: d(1) = 3*delta(1)
             d[0] = 3*delta[0]
 
         # M: d(n) = ((2*h(n-1)+h(n-2))*delta(n-1) - h(n-1)*delta(n-2))/(h(n-1)+h(n-2))
         d[nn] = ((2*h[nn-1]+h[nn-2])*delta[nn-1] - h[nn-1]*delta[nn-2])/(h[nn-1]+h[nn-2])
         # M: if isreal(d) && (sign(d(n)) ~= sign(del(n-1)))
-        if all(isreal(d)) and (sign(d[nn]) != sign(delta[nn-1])):
+        if all(np.isreal(d)) and (np.sign(d[nn]) != np.sign(delta[nn-1])):
             # M: d(n) = 0
             d[nn] = 0
         # M: elseif (sign(del(n-1)) ~= sign(del(n-2))) && (abs(d(n)) > abs(3*del(n-1)))
-        elif (sign(delta[nn-1]) != sign(delta[nn-2])) and (abs(d[nn]) > abs(3*delta[nn-1])):
+        elif (np.sign(delta[nn-1]) != np.sign(delta[nn-2])) and (abs(d[nn]) > abs(3*delta[nn-1])):
             # M: d(n) = 3*delta(n-1);
-            d[nn] = 3*delta[nn-1];
+            d[nn] = 3*delta[nn-1]
 
     # M: v = pwch(x,y,d,h,delta); # DEAD sizey = 1: v.dim = sizey;
     # v is an object describing a polynomical of order 4 with breaks = x,# v.coeffs = <computed by pwch>
@@ -180,21 +176,24 @@ def pchip(x,y,xx,expected_index=None): # pchip_m
     # M: dnm1 = d*(n-1)
     dnm1 = d*(nn) # this is a dimension, not a value
     # M: c = [reshape((dzdxdx-dzzdx)/dxd,dnm1,1),reshape(2*dzzdx-dzdxdx,dnm1,1),reshape(s(:,1:n-1),dnm1,1),reshape(y(:,1:n-1),dnm1,1)]
-    c = array([reshape((dzdxdx-dzzdx)/dxd, dnm1),
-               reshape(2*dzzdx-dzdxdx, dnm1),
-               reshape(s[0:nn], dnm1),
-               reshape(y[0:nn], dnm1)])
+    c = np.array([np.reshape((dzdxdx-dzzdx)/dxd, dnm1),
+               np.reshape(2*dzzdx-dzdxdx, dnm1),
+               np.reshape(s[0:nn], dnm1),
+               np.reshape(y[0:nn], dnm1)])
     k = 4 # order
     # end pwch
     # M: yy = ppval(v,xx); # interpolate xx by fitting polynomical described by v
     b = x # CONSIDER no duplication
     # M: l = n - 1 # pieces
-    l = nn # pieces (a dimension)
+    #l = nn # pieces (a dimension)
     # M: [ignore, index] = histc(xx,[-inf,b(2:l),inf]); # M: find the indices in b where each xx is closest
     # b(2:l) => b(2:n-1) implies we write over the first and last element with -inf/+inf
-    bins = array(b); bins[0] = -inf; bins[-1] = inf # bins (aka x) are ensured to be float so this never generates OverflowError
+    # bins (aka x) are ensured to be float so this never generates OverflowError
+    bins = np.array(b)
+    bins[0] = -np.inf
+    bins[-1] = np.inf 
     # Completely lucky find: numpy.searchsorted, which reports the indices where elements of xx need to be inserted in bins to preserve the order of bins
-    index = searchsorted(bins, xx, side='right') # NOT side='left' sg144 jun08 dive 53 fails in first salin pchip calc with first point
+    index = np.searchsorted(bins, xx, side='right') # NOT side='left' sg144 jun08 dive 53 fails in first salin pchip calc with first point
     index -= 1 # searchsorted returns indices are for the right-most edge of bins correctly but we need 'leftmost'
         
     if (expected_index is not None):
@@ -225,35 +224,16 @@ def main():
                        [1., 4., 6., -2.],
                        [1.0000,   16.0000,   35.7500,    9.3571], # !! note -2 value
                        )
+    print(y_new)
 
     y_new = pchip_test([-3., -2., -1., 0, 1., 2., 3.],
                        [-1., -1., -1., 0, 1., 1., 1.],
                        [3.01, 2.5, -3.1, 0.5, 0.8, 0.9, 1.1, 2.0, 2.1],
                        [1.0000, 1.0000, 1.0000, 0.6250, 0.9280, 0.9810, 1.0000, 1.0000, 1.0000],
                        )
-
+    print(y_new)
+    
     return None
 
 if __name__ == "__main__":
-    retval = 1
-
-    # Force to be in UTC
-    os.environ['TZ'] = 'UTC'
-    time.tzset()
-
-    try:
-        if "--profile" in sys.argv:
-            sys.argv.remove('--profile')
-            profile_file_name = os.path.splitext(os.path.split(sys.argv[0])[1])[0] + '_' \
-                + Utils.ensure_basename(time.strftime("%H:%M:%S %d %b %Y %Z", time.gmtime(time.time()))) + ".cprof"
-            # Generate line timings
-            retval = cProfile.run("main()", filename=profile_file_name)
-            stats = pstats.Stats(profile_file_name)
-            stats.sort_stats('time', 'calls')
-            stats.print_stats()
-        else:
-            retval = main()
-    except Exception:
-        log_critical("Unhandled exception in main -- exiting")
-
-    sys.exit(retval)
+    main()
