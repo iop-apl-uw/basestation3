249da77bdcde8d7463871f4ecf09f20cd8127624
Jason Gobat <jgobat@uw.edu>
2025-02-27 19:59:05 -0800

    ruff pass

--- parms.py
+++ parms.py
@@ -1,15 +1,40 @@
-# ruff: noqa
+## Copyright (c) 2023, 2025  University of Washington.
+## 
+## Redistribution and use in source and binary forms, with or without
+## modification, are permitted provided that the following conditions are met:
+## 
+## 1. Redistributions of source code must retain the above copyright notice, this
+##    list of conditions and the following disclaimer.
+## 
+## 2. Redistributions in binary form must reproduce the above copyright notice,
+##    this list of conditions and the following disclaimer in the documentation
+##    and/or other materials provided with the distribution.
+## 
+## 3. Neither the name of the University of Washington nor the names of its
+##    contributors may be used to endorse or promote products derived from this
+##    software without specific prior written permission.
+## 
+## THIS SOFTWARE IS PROVIDED BY THE UNIVERSITY OF WASHINGTON AND CONTRIBUTORS “AS
+## IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+## IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+## DISCLAIMED. IN NO EVENT SHALL THE UNIVERSITY OF WASHINGTON OR CONTRIBUTORS BE
+## LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+## CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+## GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+## HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+## LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+## OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-import pickle
-import sys
+import asyncio
 import re
-import json
+import sys
+
 import aiofiles
 import aiofiles.os
-import asyncio
-from anyio import Path
-import parmdata
 import aiosqlite
+
+import parmdata
+
 
 def rowToDict(cursor: aiosqlite.Cursor, row: aiosqlite.Row) -> dict:
     data = {}
@@ -22,7 +47,7 @@ async def state(d, logfile=None, cmdfile=None, capfile=None, dbfile=None):
     if not d:
         d = parmdata.parms
 
-    if capfile and logfile == None:
+    if capfile and logfile is None:
         currfile = capfile
         r = re.compile(r'[0-9]+.[0-9]+,SUSR,N,\$(?P<param>\w+),(?P<value>[+-]?([0-9]*[.])?[0-9]+)')
     else:
@@ -43,10 +68,10 @@ async def state(d, logfile=None, cmdfile=None, capfile=None, dbfile=None):
                                 d[p]['current'] = float(v)
                     elif line.startswith('$SENSORS'):
                         sensors = line.strip().split(',')[1:]
-                        d['SENSORS'] = sensors;
+                        d['SENSORS'] = sensors
                     elif line.startswith('$TGT_NAME'):
                         d['TGT_NAME'] = line.strip().split(',')[1]
-        except:
+        except Exception:
             pass
 
     elif dbfile and await aiofiles.os.path.exists(dbfile):
@@ -64,7 +89,7 @@ async def state(d, logfile=None, cmdfile=None, capfile=None, dbfile=None):
                     d['SENSORS'] = latest['log_SENSORS'].split(',')
                 if 'log_TGT_NAME' in latest:
                     d['TGT_NAME'] = latest['log_TGT_NAME']
-            except:
+            except Exception:
                 pass
     
     if cmdfile and await aiofiles.os.path.exists(cmdfile):
@@ -78,18 +103,18 @@ async def state(d, logfile=None, cmdfile=None, capfile=None, dbfile=None):
                             if d[p]['type'] == 'INT':
                                 try:
                                     n = int(v)
-                                except:
+                                except Exception:
                                     continue
                             else:
                                 try:
                                     n = float(v)
-                                except:
+                                except Exception:
                                     continue
  
                             if f"{d[p]['current']}" != f"{n}":
                                 print(f"changing {p} {d[p]['current']} to {n}")
                                 d[p]['waiting'] = n
-        except:
+        except Exception:
             pass
 
     if 'SENSORS' in d and d['SENSORS'] and 'LOGGERS' in d:
@@ -98,7 +123,7 @@ async def state(d, logfile=None, cmdfile=None, capfile=None, dbfile=None):
         d['LOGGERS']['bitfield'] = [{'value': 1, 'function': loggers[0]}, 
                                     {'value': 2, 'function': loggers[1]}, 
                                     {'value': 4, 'function': loggers[1]}, 
-                                    {'value': 8, 'function': loggers[1]}];
+                                    {'value': 8, 'function': loggers[1]}]
     
     return d
   
@@ -107,7 +132,7 @@ async def parameterChanges(dive, logname, cmdname):
     p = await state(None, logfile=logname, cmdfile=cmdname)
 
     changes = []
-    for x in p.keys():
+    for x in p:
         if 'waiting' in p[x]:
             changes.append(  { "dive": dive, "parm": x, "oldval": p[x]['current'], "newval": p[x]['waiting'] } )
 
--- scicon.py
+++ scicon.py
@@ -1,8 +1,36 @@
-import sys
+## Copyright (c) 2023, 2025  University of Washington.
+## 
+## Redistribution and use in source and binary forms, with or without
+## modification, are permitted provided that the following conditions are met:
+## 
+## 1. Redistributions of source code must retain the above copyright notice, this
+##    list of conditions and the following disclaimer.
+## 
+## 2. Redistributions in binary form must reproduce the above copyright notice,
+##    this list of conditions and the following disclaimer in the documentation
+##    and/or other materials provided with the distribution.
+## 
+## 3. Neither the name of the University of Washington nor the names of its
+##    contributors may be used to endorse or promote products derived from this
+##    software without specific prior written permission.
+## 
+## THIS SOFTWARE IS PROVIDED BY THE UNIVERSITY OF WASHINGTON AND CONTRIBUTORS “AS
+## IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+## IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+## DISCLAIMED. IN NO EVENT SHALL THE UNIVERSITY OF WASHINGTON OR CONTRIBUTORS BE
+## LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+## CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+## GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+## HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+## LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+## OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 import asyncio
+import re
+
 import aiofiles
 from anyio import Path
-import re
+
 
 def parseBody(body, uniq=True):
     exp = re.compile(r'(?P<lead>^#[\S\s]*?)?(?P<inst>^[A-Za-z0-9_]+)\s+=\s+{$\s+(?P<def>[^}]*)}$', re.MULTILINE)
@@ -81,9 +109,9 @@ async def state(base):
         scheme = await fromFile(str(schFiles[0]), uniq=False)
         schSrc = schFiles[0].name
 
-    for k in attach.keys():
-        attach[k]['prop'] = inst[attach[k]['type']] if attach[k]['type'] in inst else None
-        attach[k]['scheme'] = scheme[k] if k in scheme else []
+    for k in attach:
+        attach[k]['prop'] = inst.get(attach[k]['type'], None)
+        attach[k]['scheme'] = scheme.get(k, [])
   
     return (attach, insSrc, attSrc, schSrc)
 
