304feb218ed333b74266b05eb264b490141c1c20
Geoff Shilling <gbs2@uw.edu>
2026-01-28 15:32:52 -0800

    Check for cases where there are no valid total_flight_time_s for the disk end date calculation

--- Plotting/MissionDisk.py
+++ Plotting/MissionDisk.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 # -*- python-fmt -*-
 
-## Copyright (c) 2023, 2024, 2025  University of Washington.
+## Copyright (c) 2023, 2024, 2025, 2026  University of Washington.
 ##
 ## Redistribution and use in source and binary forms, with or without
 ## modification, are permitted provided that the following conditions are met:
@@ -64,7 +64,8 @@ def est_remaining(df, y_val, tag_val: str) -> str:
             y_val[dives_back:],
             1,
         )
-
+    if df["total_flight_time_s"][dives_back:].isna().all():
+        return ""
     dives_left = -b / m
     secs_remaining = np.nanmean(df["total_flight_time_s"][dives_back:]) * dives_left
 
